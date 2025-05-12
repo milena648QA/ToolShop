02da315e53c8fa036b118773ae037c37cd63a679
milena648 <milenastojanovic2112@gmail.com>
2025-05-12 18:51:06 +0200

    update

--- src/test/java/tests/FavoritesAPITest.java
+++ src/test/java/tests/FavoritesAPITest.java
@@ -1,14 +1,27 @@
 package tests;
 
 import io.restassured.response.Response;
+import lombok.SneakyThrows;
 import org.junit.Assert;
 import org.junit.Test;
 import pages.FavoritesAPIPage;
+import pages.UserAPIPage;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class FavoritesAPITest {
 
+    @SneakyThrows
     @Test
     public void retrieveAllFavorites() {
+        String bodyText = Files.readString(Paths.get("src/test/resources/loginCustomer.json"));
+        String bodyText2 = Files.readString(Paths.get("src/test/resources/user.json"));
+
+        UserAPIPage userAPIPage = new UserAPIPage();
+        userAPIPage.storeNewUser(bodyText2);
+        userAPIPage.loginCustomer(bodyText);
+
         FavoritesAPIPage favoritesAPIPage = new FavoritesAPIPage();
         Response response = favoritesAPIPage.retrieveAllFavorites();
 
